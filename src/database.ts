42a0e35c57f57c43b89060d344377ac5f5146ef6
akshayaxo <akshayv0506@gmail.com>
2022-05-06 16:05:50 +0100

    added in memory database to retrieve data from already added data.

--- src/database.ts
+++ src/database.ts
@@ -1,22 +1,27 @@
-interface Reading {
-  // TODO: change this to contain whatever information is needed
+export interface Reading {
+    time: string;
+    name: string;
+    value: number;
 }
 
 // This is a fake database which stores data in-memory while the process is running
 // Feel free to change the data structure to anything else you would like
-const database: Record<string, Reading> = {};
+const database: Record<string, Reading[]> = {};
 
 /**
  * Store a reading in the database using the given key
  */
-export const addReading = (key: string, reading: Reading): Reading => {
-  database[key] = reading;
-  return reading;
+export const addReading = (key: string, reading: Reading[]): boolean => {
+    database.key = reading;
+    console.log(key);
+    console.log(database.key);
+    return true;
 };
 
 /**
  * Retrieve a reading from the database using the given key
  */
-export const getReading = (key: string): Reading | undefined => {
-  return database[key];
+export const getReading = (key: string): Reading[] | undefined => {
+    console.log(database.key);
+    return database.key;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express, { Express } from 'express';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
-import { addReading, getReading } from './database';
+import { addReading, getReading, Reading } from './database';
+import { isString } from 'util';
 
 dotenv.config();
 
@@ -15,19 +16,55 @@ app.use(express.urlencoded({ extended: true }));
 app.post('/data', async (req, res) => {
   // TODO: parse incoming data, and save it to the database
   // data is of the form:
-  //  {timestamp} {name} {value}
-
-  // addReading(...)
-
-  return res.json({ success: false });
+    //  {timestamp} {name} {value}
+    let datas: Reading[] = [];
+    var result = true;
+    // Splitting the incoming text format data and forming a list out of it which contains each reading as a single item.
+    const incomingdatas = req.body.toString().replace(/\r\n/g, '\n').split('\n');
+    // looping through the entire list of data and then identifying each individual entity
+    for (let incomingdata of incomingdatas) {
+        var splitteddata = incomingdata.split(" ", 3);
+        var datadate = new Date(parseInt(splitteddata[0]));
+        var value = parseFloat(splitteddata[2]);
+        try {
+            if (!isNaN(value) && isString(splitteddata[1])) {
+                let data: Reading = {
+                    time: datadate.toISOString(),
+                    name: splitteddata[1],
+                    value: value
+                };
+                datas.push(data);
+            }
+            else {
+                result = false;
+                break;
+            }
+        }
+        catch {
+            result = false;
+        }  
+    }
+    if (result) {
+        for (var data of datas) {
+            addReading((data.time).split("T")[0], datas);
+        }
+    }
+    return res.json({ success: result });
 });
 
 app.get('/data', async (req, res) => {
-  // TODO: check what dates have been requested, and retrieve all data within the given range
-
-  // getReading(...)
-
-  return res.json({ success: false });
+    // TODO: check what dates have been requested, and retrieve all data within the given range
+    const fromDate: string = req.query.from as string;
+    const toDate: string = req.query.to as string;
+    let retrieveddatas: Reading[] = [];
+    for (var d = new Date(fromDate); d <= new Date(toDate); d.setDate(d.getDate() + 1)) {
+        var data = getReading(d.toDateString());
+        if (data != null) {
+            retrieveddatas = data;
+        }
+    }
+    console.log(JSON.stringify(retrieveddatas));
+    return res.json({ success: true });
 });
 
 app.listen(PORT, () => console.log(`Running on port ${PORT} ⚡`));
